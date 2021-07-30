d4cd84c0157cc78531ea5fdedda904615884e859
marcelropos <Marcel.R0@outlook.de>
2021-07-30 12:29:51 +0200

    provide the collection names global

---
+++ core/globalenum.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class CollectionEnum(Enum):
+    ROLES = "roles"
+    CHANNELS = "channels"
+    MESSAGES = "messages"
+    AUDIO_FILES = "audioFiles"
+    EMOJIS = "emojis"
+    MODERATOR_SETTINGS = "moderatorSetting"
+    ROLES_SETTINGS = "RolesSetting"
