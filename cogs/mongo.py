87e18bf7286b91e944302939ddde847647c31530
marcelropos <Marcel.R0@outlook.de>
2021-08-07 23:58:44 +0200

    improve database actions

--- cogs/mongo.py
+++ cogs/mongo.py
@@ -1,7 +1,14 @@
-from discord.ext.commands import Cog, Bot
+from datetime import datetime
+from distutils.util import strtobool
+from typing import Union
+
+from discord import Embed
+from discord.ext.commands import Cog, Bot, BadArgument
 from discord.ext.commands import group, Context
 
+from core.error.error_collection import CouldNotEditEntryError
 from core.globalEnum import CollectionEnum
+# noinspection PyUnresolvedReferences
 from mongo.primitiveMongoData import PrimitiveMongoData
 
 
@@ -14,41 +21,171 @@ class Mongo(Cog):
     def __init__(self, bot: Bot):
         self.bot: Bot = bot
 
-    # Roles
-
     @group(pass_context=True)
     async def mongo(self, ctx: Context):
         if not ctx.invoked_subcommand:
-            raise ModuleError
+            raise BadArgument
 
     @mongo.group(pass_context=True,
                  name="add")
-    async def mongo_add(self, ctx: Context, collection: CollectionEnum, key: str, value: str):
-        await PrimitiveMongoData(collection).insert_one({key: value})
+    async def mongo_add(self, ctx: Context, collection: CollectionEnum, key: str, *, value: str):
+        """
+        Adds a key value pair to the collection.
+
+        Args:
+            ctx: The command context provided by the discord.py wrapper.
+
+            collection:  The collection (you can say a database table) which shall now contain the key value pair.
+
+            key: The key of the pair.
+
+            value: The value the pair.
+
+        Replies:
+            The user will receive a reply which contains representation of the data record.
+        """
+        converted_value = self.converter(value)
+        respond = await PrimitiveMongoData(collection).insert_one({key: converted_value})
+
+        embed = Embed(title=f"Database.{collection.value}",
+                      description=f"The following data record is now saved in the database.\n"
+                                  f"```\n{respond}\n```")
+        await ctx.reply(embed=embed, delete_after=600)
 
     @mongo.group(pass_context=True,
                  name="find")
     async def mongo_find(self, ctx: Context, collection: CollectionEnum, key: str):
+        """
+        Finds a record in a specific collection.
+
+        Args:
+            ctx: The command context provided by the discord.py wrapper.
+
+            collection: The collection (you can say a database table) which shall contain the key value pair.
+
+            key: The key which is used to search for the record.
+
+        Replies:
+            The simple readable representation of the record or a not found message.
+
+        """
         found = await PrimitiveMongoData(collection).find_one({key: {"$exists": True}})
+        if found:
+
+            embed = Embed(title=f"Database.{collection.value}",
+                          description=f'```\n"{key}" : {self.display_value(found[key])}\n```')
+        else:
+            embed = Embed(title=f"Database.{collection.value}",
+                          description=f"It seems that there is no such entry in this collection.\n"
+                                      f"Did you choose the wrong collection?")
+        await ctx.reply(embed=embed, delete_after=600)
 
     @mongo.group(pass_context=True,
                  name="edit")
-    async def mongo_edit(self, ctx: Context, collection: CollectionEnum, key: str, value: str):
-        await PrimitiveMongoData(collection).update_one({key: {"$exists": True}}, {key: value})
+    async def mongo_edit(self, ctx: Context, collection: CollectionEnum, key: str, *, value: str):
+        """
+        Edits a existing record.
+
+        Args:
+            ctx: The command context provided by the discord.py wrapper.
+
+            collection: The collection (you can say a database table) which shall contain the key value pair.
+
+            key: The key which es used to find the record.
+
+            value: The replacing value.
+
+        Replies:
+            A diff which contains the old and new pair.
+        """
+        converted_value = self.converter(value)
+        found = await PrimitiveMongoData(collection).find_one({key: {"$exists": True}})
+        if found:
+            await PrimitiveMongoData(collection).update_one({key: {"$exists": True}}, {key: converted_value})
+
+            embed = Embed(title=f"Database.{collection.value}",
+                          description='```diff\n'
+                                      f'- "{key}" : "{self.display_value(found[key])}"\n'
+                                      f'+ "{key}" : "{self.display_value(value)}"\n'
+                                      f'```')
+            await ctx.reply(embed=embed, delete_after=600)
+
+        else:
+            raise CouldNotEditEntryError(collection, key, value)
 
     @mongo.group(pass_context=True,
                  name="remove",
                  aliases=["rem", "rm"])
     async def mongo_remove(self, ctx: Context, collection: CollectionEnum, key: str):
-        await PrimitiveMongoData(collection).delete_one({key: {"$exists": True}})
+        """
+        Removes any records which has a specific key.
 
-    # Converter
+        Args:
+            ctx: he command context provided by the discord.py wrapper.
 
-    async def cog_before_invoke(self, ctx: Context):
-        pass
+            collection: The collection (you can say a database table) which shall not longer contain the key value pair.
 
-    async def cog_after_invoke(self, ctx: Context):
-        pass
+            key: The key which shall wiped out.
+
+        Replies:
+            A message that (obviously) informs you that the records have been wiped.
+        """
+        await PrimitiveMongoData(collection).delete_many({key: {"$exists": True}})
+
+        embed = Embed(title=f"Database.{collection.value}",
+                      description="I do not know if such an entry was existing, if it was, it is now gone.")
+        await ctx.reply(embed=embed, delete_after=600)
+
+    def display_value(self, value: Union[str, bool, datetime]) -> str:
+        """
+        Decides whether the value should be returned in quotation marks or not.
+
+        Args:
+            value: The value to be printed properly.
+
+        Returns:
+            The proper string.
+        """
+        converted_value = self.converter(value)
+        if isinstance(converted_value, bool):
+            value_str = str(converted_value)
+        elif isinstance(value, datetime):
+            value_str = converted_value
+        else:
+            value_str = f'"{value}"'
+        return value_str
+
+    @staticmethod
+    def converter(data: Union[str, bool, datetime]) -> Union[str, bool, datetime]:
+        """
+        Converts text input data to storable objects and creates output text from objects.
+
+        Text that represents a boolean value is converted to a boolean.
+        Text representing a date & time is converted to a date object.
+        A date object is converted as a simple readable text.
+        Everything else is equivalent to the input.
+
+        Args:
+            data: The string to be converted
+
+        Returns:
+            The representing Object
+
+        """
+        if isinstance(data, str):
+            try:
+                result = bool(strtobool(data))
+            except ValueError:
+                try:
+                    result = datetime.strptime(data, '%d.%m.%y %H:%M')
+                except ValueError:
+                    result = data
+
+        elif isinstance(data, datetime):
+            result = data.strftime('%d.%m.%y %H:%M')
+        else:
+            result = data
+        return result
 
 
 def setup(bot: Bot):
