f702e9b24e178d28199c7a7b0d369d9814fb5c16
marcelropos <Marcel.R0@outlook.de>
2021-08-07 21:51:48 +0200

    implement BadArgumentHandler and EntryNotFoundErrorHandler

--- core/error/error_collection.py
+++ core/error/error_collection.py
@@ -1,2 +1,12 @@
+from core.globalEnum import CollectionEnum
+
+
 class ManPageNotFound(Exception):
     pass
+
+
+class CouldNotEditEntryError(Exception):
+    def __init__(self, collection: CollectionEnum, key: str, value: str = "<value>"):
+        self.collection = collection
+        self.key = key
+        self.value = value
