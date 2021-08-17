571becb8ab3c6e28d60be0541bfbfe565eb05622
marcelropos <Marcel.R0@outlook.de>
2021-08-17 20:58:39 +0200

    start implement predicates

---
+++ core/error/handler/no_multible_groups_handler.py
@@ -0,0 +1,26 @@
+from logging import Logger
+from typing import Callable
+
+from core.error.error_collection import NoMultipleGroupsError
+from core.error.handler.base_handler import BaseHandler
+from core.logger import get_discord_child_logger
+
+
+class NoMultipleGroupsHandler(BaseHandler):
+    error: NoMultipleGroupsError
+
+    @staticmethod
+    def handles_type():
+        return NoMultipleGroupsError
+
+    @property
+    def cause(self) -> str:
+        return f'You may only belong to one group, but you already assigned to <@&{self.error.role.id}>.'
+
+    @property
+    async def solution(self) -> str:
+        return f"Ask an Admin to reassign you."
+
+    @property
+    def logger(self) -> Callable[[str], Logger]:
+        return get_discord_child_logger
