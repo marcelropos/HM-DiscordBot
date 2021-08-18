1d00884b9e442c19993c7cd9d448e11a07480d63
marcelropos <Marcel.R0@outlook.de>
2021-08-18 19:24:38 +0200

    handle errors. create logs and notify the server in case of an error.

--- cogs/util/accepted_chats.py
+++ cogs/util/accepted_chats.py
@@ -1,8 +1,16 @@
+from typing import Optional
+
 from discord import TextChannel
 from discord.ext.commands import Bot
+from pymongo.errors import ServerSelectionTimeoutError
 
+from core.error.error_collection import BrokenConfigurationError
+from core.error.error_reply import startup_error_reply
 from core.globalEnum import CollectionEnum, ConfigurationNameEnum
+from core.logger import get_discord_child_logger, get_mongo_child_logger
 from mongo.primitiveMongoData import PrimitiveMongoData
+
+error_msg = "Due to the previous reason, all commands limited to chats are out of function!"
 
 
 async def assign_accepted_chats(bot: Bot, channels: set[TextChannel]):
@@ -14,9 +22,38 @@ async def assign_accepted_chats(bot: Bot, channels: set[TextChannel]):
 
         channels: A set in which the accepted chats should be stored.
     """
-    channels.clear()
-    db = PrimitiveMongoData(CollectionEnum.CHANNELS)
-    key = ConfigurationNameEnum.BOT_COMMAND_CHAT.value
-    channels.add(bot.get_channel((await db.find_one({key: {"$exists": True}}))[key]))
-    key = ConfigurationNameEnum.DEBUG_CHAT.value
-    channels.add(bot.get_channel((await db.find_one({key: {"$exists": True}}))[key]))
+    try:
+
+        channels.clear()
+        db = PrimitiveMongoData(CollectionEnum.CHANNELS)
+
+        command_key = ConfigurationNameEnum.BOT_COMMAND_CHAT.value
+        command: Optional[dict] = await db.find_one({command_key: {"$exists": True}})
+        debug_key = ConfigurationNameEnum.DEBUG_CHAT.value
+        debug: Optional[dict] = await db.find_one({debug_key: {"$exists": True}})
+
+        if command and debug:
+            channels.add(bot.get_channel(command[command_key]))
+            channels.add(bot.get_channel(debug[debug_key]))
+        else:
+            raise BrokenConfigurationError
+
+    except (TypeError, BrokenConfigurationError):
+        cause = "The bot chat configuration is broken."
+        await startup_error_reply(bot=bot,
+                                  title="Invalid command Chat configuration.",
+                                  cause=cause,
+                                  solution=f"Please check {ConfigurationNameEnum.BOT_COMMAND_CHAT} "
+                                           f"& {ConfigurationNameEnum.DEBUG_CHAT} and reload all modules.")
+
+        get_discord_child_logger(__name__).error(f"{cause} {error_msg}")
+
+    except ServerSelectionTimeoutError:
+        title = "Error on Startup"
+        cause = "Could not connect to database."
+        solution = f"```\n" \
+                   f"1. Establish the connection to the database.\n" \
+                   f"2. Reload all modules." \
+                   f"```"
+        get_mongo_child_logger(__name__).error(f"{cause} {error_msg}")
+        await startup_error_reply(bot=bot, title=title, cause=cause, solution=solution)
