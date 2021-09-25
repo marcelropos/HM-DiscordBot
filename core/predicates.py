af5c263e1101cf4e460cd93db6d7b4dc847f9feb
Maximilian Waidelich <maximilian.waidelich@gmail.com>
2021-09-25 21:17:28 +0200

    maybe fixed predicates

--- core/predicates.py
+++ core/predicates.py
@@ -46,9 +46,16 @@ def has_role_plus(item: Union[Placeholder, set[Role]]):
         if not item:
             raise NoRulesError
 
-        if {_ for _ in set(ctx.author.roles) if _ in item}:
+        if isinstance(item, Placeholder):
+            _item = {item}
+        else:
+            _item = item
+
+        _item: set[Union[Role, Placeholder]]
+
+        if {_ for _ in set(ctx.author.roles) if _ in _item}:
             return True
         else:
-            raise BotMissingRole([role.name for role in item])
+            raise BotMissingRole([role.name for role in _item])
 
     return check(predicate)
