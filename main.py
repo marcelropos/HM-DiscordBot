ffebf2cdb89ad3fa4366168254acf247f4941cc8
Maximilian Waidelich <maximilian.waidelich@gmail.com>
2022-08-21 12:50:20 +0200

    fixed cog loading

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import asyncio
 import os
 
 import discord
@@ -14,10 +15,16 @@ intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="!", case_insensitive=True, intents=intents)
 bot.help_command = PrettyHelp(show_index=True, sort_commands=True, no_category=True, color=0x00f2ff)
 
-for filename in os.listdir("./cogs"):
-    if filename.endswith(".py") and filename != "upgrade.py":
-        await bot.load_extension(f"cogs.{filename[:-3]}")
-        logger.get_discord_child_logger("cogs").info(f"Loaded: cogs.{filename[:-3]}")
+
+async def load_cogs():
+    async with bot:
+        for filename in os.listdir("./cogs"):
+            if filename.endswith(".py") and filename != "upgrade.py":
+                await bot.load_extension(f"cogs.{filename[:-3]}")
+                logger.get_discord_child_logger("cogs").info(f"Loaded: cogs.{filename[:-3]}")
+
+
+asyncio.run(load_cogs())
 
 
 @bot.event
