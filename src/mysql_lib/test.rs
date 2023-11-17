e1117c29f404d2fafe571bfd14536265c4604b3a
Maximilian Waidelich <maximilian.waidelich@gmail.com>
2023-11-17 21:49:29 +0100

    extracted method

--- src/mysql_lib/test.rs
+++ src/mysql_lib/test.rs
@@ -1,14 +1,21 @@
 #[cfg(test)]
 mod tests {
     use poise::serenity_prelude::GuildId;
+    use sqlx::{MySql, Pool};
 
-    use crate::mysql_lib::{get_connection, is_guild_in_database};
+    use crate::mysql_lib::{get_connection, is_guild_in_database, migrate_database};
 
-    #[tokio::test]
-    async fn test_is_guild_in_database() {
+    async fn get_connection_pool() -> Pool<MySql>{
         let pool = get_connection(1)
             .await
             .expect("Database could not be connected to");
+        migrate_database(&pool).await.expect("Database migration could not be done");
+        return pool;
+    }
+
+    #[tokio::test]
+    async fn test_is_guild_in_database() {
+        let pool = get_connection_pool().await;
         let result = is_guild_in_database(&pool, GuildId(1))
             .await
             .expect("Query was not successful");
