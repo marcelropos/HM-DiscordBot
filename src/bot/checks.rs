a17e7fb121819a0c08b261b75be51c927cfbf8ea
Maximilian Waidelich <maximilian.waidelich@gmail.com>
2023-12-29 12:18:34 +0100

    fixes

--- src/bot/checks.rs
+++ src/bot/checks.rs
@@ -1,3 +1,4 @@
+use once_cell::sync::Lazy;
 use poise::serenity_prelude::{ChannelId, Permissions, RoleId};
 use sqlx::{MySql, Pool};
 use std::env;
@@ -6,10 +7,24 @@ use tracing::error;
 use crate::bot::Context;
 use crate::mysql_lib;
 
+static MAIN_GUILD_ID: Lazy<Option<u64>> = Lazy::new(|| match env::var("MAIN_GUILD_ID") {
+    Ok(val) => match val.parse() {
+        Ok(val) => Some(val),
+        Err(err) => {
+            error!(error = err.to_string(), "Failed to parse int");
+            None
+        }
+    },
+    Err(_) => {
+        error!("No Main Guild ID given");
+        None
+    }
+});
+
 /// Checks if the author has any of the specified roles
 ///
 /// Will return false when:
-/// - the message was not send in a guild
+/// - the message was not sent in a guild
 /// - the author doesn't have any of the roles
 pub async fn has_one_of_roles(ctx: Context<'_>, roles: Vec<RoleId>) -> bool {
     if let Some(member) = ctx.author_member().await {
@@ -21,37 +36,27 @@ pub async fn has_one_of_roles(ctx: Context<'_>, roles: Vec<RoleId>) -> bool {
 /// Checks if the author has the specified role
 ///
 /// Will return false when:
-/// - the message was not send in a guild
+/// - the message was not sent in a guild
 /// - the author doesn't the role
 #[allow(dead_code)]
 pub async fn has_role(ctx: Context<'_>, role: RoleId) -> bool {
     has_one_of_roles(ctx, vec![role]).await
 }
 
-/// Checks that the author doesn't have any the specified role
+/// Checks that the author doesn't have any of the specified roles
 ///
 /// Will return true when:
-/// - the message was not send in a guild
+/// - the message was not sent in a guild
 /// - the author doesn't have any of the role
 #[allow(dead_code)]
-pub async fn has_not_roles(ctx: Context<'_>, roles: Vec<RoleId>) -> bool {
+pub async fn has_none_of_roles(ctx: Context<'_>, roles: Vec<RoleId>) -> bool {
     !has_one_of_roles(ctx, roles).await
-}
-
-/// Checks that the author doesn't have the specified role
-///
-/// Will return true when:
-/// - the message was not send in a guild
-/// - the author doesn't have the role
-#[allow(dead_code)]
-pub async fn has_not_role(ctx: Context<'_>, role: RoleId) -> bool {
-    has_not_roles(ctx, vec![role]).await
 }
 
 /// Checks if the author has a role that gives him admin permissions
 ///
 /// Will return false when:
-/// - the message was not send in a guild
+/// - the message was not sent in a guild
 /// - the author doesn't have a role with admin permissions
 #[allow(dead_code)]
 pub async fn is_admin(ctx: Context<'_>) -> bool {
@@ -67,11 +72,11 @@ pub async fn is_admin(ctx: Context<'_>) -> bool {
     false
 }
 
-/// Checks if the author is the owner of the guild where the message was send
+/// Checks if the author is the owner of the guild where the message was sent
 ///
 /// Will return false when:
-/// - the message was not send in a guild
-/// - the message was send by the guild owner
+/// - the message was not sent in a guild
+/// - the message was sent by the guild owner
 #[allow(dead_code)]
 pub async fn is_owner(ctx: Context<'_>) -> bool {
     if let Some(guild) = ctx.guild() {
@@ -80,58 +85,51 @@ pub async fn is_owner(ctx: Context<'_>) -> bool {
     false
 }
 
-/// Checks if the message was send in the main guild
+/// Checks if the message was sent in the main guild
 ///
 /// Will return false when:
 /// - guild is not main guild
 /// - guild id env variable is not set
 /// - guild id env variable can't be parsed to u64
-/// - message was not send in guild
+/// - message was not sent in guild
 #[allow(dead_code)]
-pub async fn send_in_main_guild(ctx: Context<'_>) -> bool {
+pub async fn sent_in_main_guild(ctx: Context<'_>) -> bool {
     if let Some(guild) = ctx.guild() {
-        let main_guild_id: u64 = match env::var("MAIN_GUILD_ID") {
-            Ok(val) => match val.parse() {
-                Ok(val) => val,
-                Err(err) => {
-                    error!(error = err.to_string(), "Failed to parse int");
-                    return false;
-                }
-            },
-            Err(_) => {
-                error!("No Main Guild ID given");
-                return false;
-            }
-        };
-        return guild.id.0 == main_guild_id;
+        return MAIN_GUILD_ID.map_or(false, |main_guild_id|  guild.id.0 == main_guild_id);
     }
     false
 }
 
-/// Checks if the message was send in one of the allowed channels
+/// Checks if the message was sent in one of the allowed channels
 #[allow(dead_code)]
-pub async fn send_in_channels(ctx: Context<'_>, channels: Vec<ChannelId>) -> bool {
+pub async fn sent_in_channels(ctx: Context<'_>, channels: Vec<ChannelId>) -> bool {
     channels.contains(&ctx.channel_id())
 }
 
-/// Checks if the message was send in the wanted channel
+/// Checks if the message was sent in the wanted channel
 #[allow(dead_code)]
-pub async fn send_in_channel(ctx: Context<'_>, channel: ChannelId) -> bool {
+pub async fn sent_in_channel(ctx: Context<'_>, channel: ChannelId) -> bool {
     ctx.channel_id() == channel
 }
 
-/// Checks if the message was send in a guild that is already setup
+/// Checks if the message was sent in a guild that is already setup
 ///
 /// Will return false if:
-/// - the message was not send in a guild
+/// - the message was not sent in a guild
 /// - the guild is not in the database
 /// - the sql query had some kind of problem
 #[allow(dead_code)]
-pub async fn send_in_setup_guild(ctx: Context<'_>, pool: &Pool<MySql>) -> bool {
+pub async fn sent_in_setup_guild(ctx: Context<'_>, pool: &Pool<MySql>) -> bool {
     if let Some(guild_id) = ctx.guild_id() {
         return mysql_lib::is_guild_in_database(pool, guild_id)
             .await
             .unwrap_or(false);
     }
     false
+}
+
+/// Checks if the message was sent in a guild
+#[allow(dead_code)]
+pub async fn sent_in_guild(ctx: Context<'_>) -> bool {
+    ctx.guild_id().is_some()
 }
